abce855263f7e764fffac45d10a177a0bd447bb0
Bryan Catanzaro <bcatanzaro@acm.org>
2012-01-03 10:30:22 -0800

    Added more cast operators.

--- library/prelude/cast.h
+++ library/prelude/cast.h
@@ -1,5 +1,29 @@
 #pragma once
 
+template<typename T>
+__host__ __device__
+int op_int32(const T&x) {
+    return int(x);
+}
+
+template<typename T>
+__host__ __device__
+int op_int64(const T&x) {
+    return long(x);
+}
+
+template<typename T>
+__host__ __device__
+int op_float32(const T&x) {
+    return float(x);
+}
+
+template<typename T>
+__host__ __device__
+int op_float64(const T&x) {
+    return double(x);
+}
+
 template<typename T, typename U>
 __host__ __device__
 U cast_to(const T& x, const U& y) {
@@ -12,9 +36,48 @@ typename U::value_type cast_to_el(const T& x, const U& y) {
     return typename U::value_type(x);
 }
 
+
+template<typename T>
+class fnop_int32 {
+    typedef int return_type;
+    __host__ __device__
+    int operator()(const T& x) {
+        return op_int32(x);
+    }
+};
+
+
+template<typename T>
+class fnop_int64 {
+    typedef long return_type;
+    __host__ __device__
+    long operator()(const T& x) {
+        return op_int64(x);
+    }
+};
+
+template<typename T>
+class fnop_float32 {
+    typedef float return_type;
+    __host__ __device__
+    float operator()(const T& x) {
+        return op_int32(x);
+    }
+};
+
+template<typename T>
+class fnop_float64 {
+    typedef double return_type;
+    __host__ __device__
+    double operator()(const T& x) {
+        return op_int32(x);
+    }
+};
+
 template<typename T, typename U>
 class fncast_to {
     typedef U return_type;
+    __host__ __device__
     U operator()(const T& x, const U& y) {
         return cast_to(x, y);
     }
@@ -23,6 +86,7 @@ class fncast_to {
 template<typename T, typename U>
 class fncast_to_el {
     typedef typename U::value_type return_type;
+    __host__ __device__
     U operator()(const T& x, const U& y) {
         return cast_to_el(x, y);
     }
