b2a9bb66ed10b27ce8ac85c9a4b4c27cf34122f9
Bryan Catanzaro <bcatanzaro@acm.org>
2012-01-03 10:47:15 -0800

    Radix sort now working with literal cast inference.

--- library/thrust_wrappers/index_sequence.h
+++ library/thrust_wrappers/index_sequence.h
@@ -5,6 +5,6 @@
 struct index_sequence : public iterator_sequence<thrust::counting_iterator<long> >
 {
   __host__ __device__ index_sequence(long length) :
-  iterator_sequence<thrust::counting_iterator<long> >(thrust::counting_iterator<int>(0), length) {}
+  iterator_sequence<thrust::counting_iterator<long> >(thrust::counting_iterator<long>(0), length) {}
 };
     
